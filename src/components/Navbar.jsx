c3d3542824b539ea4320a743c82f4256cb564f7f
Arun parsad <Ptech784@gmail.com>
2023-04-17 06:46:37 +0530

    adding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import { BsPlayCircle } from 'react-icons/bs'
 import { BiHomeSmile, BiSearch, BiMovie, BiTv } from 'react-icons/bi'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
       <nav className='h-screen bg-transparent z-50 md:w-[10rem] w-20  fixed'>
-        <div className='py-4 flex flex-col justify-center items-center  space-y-[0.2rem] hover:cursor-pointer'>
-          <BsPlayCircle className='text-5xl text-[#D9D9D9]' />
-          <span className=' text-[#D9D9D9] text-md tracking-wide'>
-            MagicFlix
-          </span>
-        </div>
+        <Link to='/'>
+          <div className='py-4 flex flex-col justify-center items-center  space-y-[0.2rem] hover:cursor-pointer'>
+            <BsPlayCircle className='text-5xl text-[#D9D9D9]' />
+            <span className=' text-[#D9D9D9] text-md tracking-wide'>
+              MagicFlix
+            </span>
+          </div>
+        </Link>
         <div className='h-full flex justify-start items-center flex-col'>
           <div
             className='flex flex-col justify-between items-center list-none text-white 
